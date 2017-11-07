3d6d35aee0ede226e96bde37840f6c8a364ca403
co5alt <dylan.covalt@gmail.com>
2017-11-07 14:39:44 -0600

    Added arc motion

--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
-var time = Date.now()/1000;
-var omaha_url = "https://api.darksky.net/forecast/5b6168fb3f456887c8bb317aec03b9e3/41.2524,95.9980";
+var omaha_url = "http://api.openweathermap.org/data/2.5/weather?lat=42.25&lon=95.99&APPID=25de4193d974875b028cb9b0b62ea042";
+var time;
+var sunrise;
+var sunset;
+var day_length;
+var day_remaining;
+var position;
+var rotation;
 
-function get_times(url) {
-    $.getJSON(url, function (result) {
-        var sunrise = result.daily.data.sunriseTime;
-        var sunset = result.daily.data.sunsetTime;
-       
+
+$(document).ready(function () {
+
+
+    $.getJSON(omaha_url, function (data) {
+        time = data.dt;
+        sunrise = data.sys.sunrise + 46800;
+        sunset = data.sys.sunset + 46800;
         $("#sunrise").text(sunrise);
         $("#current").text(time);
         $("#sunset").text(sunset);
-         if (time >= sunrise && time < sunset) {
+        day_length = sunset - sunrise;
+        day_remaining = sunset - time;
+        position = 100 - Math.round(day_remaining / day_length * 100);
+        rotation = position * 1.8;
+
+
+        if (time >= sunrise && time < sunset) {
             $("body").css("background-color", "skyblue");
+            $("body").css("color", "yellow");
+            $("#fulcrum").css("transform", "rotate(" + rotation + "deg)");
         } else {
             $("body").css("background-color", "midnightblue");
+            $("body").css("color", "gray");
+            $(".sun").css("display", "hidden");
         }
-        
-
-       
     });
 
-}
-
-$(document).ready(function () {
-    get_times(omaha_url);
-
 });
-
-
-//25de4193d974875b028cb9b0b62ea042
